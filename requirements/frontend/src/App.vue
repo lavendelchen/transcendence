e31cf60cac2d8e9c5e175a399fcd4d5b7b7addf2
Nico Haas <nhaas@1-C-3.42heilbronn.de>
2023-09-18 17:30:22 +0200

    added routing & playpage.vue

---
+++ requirements/frontend/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+</script>
+
+<template>
+  <router-view></router-view>
+</template>
+
+<style scoped>
+@font-face {
+    font-family: 'textfont';
+    src: url('/fonts/Pixeled.ttf');
+}
+@font-face {
+  font-family: 'logofont';
+  src: url('/fonts/gameboy.ttf');
+}
+
+body {
+  font-family: 'textfont', sans-serif;
+}
+
+p {
+  font-family: 'textfont', sans-serif;
+}
+
+h1 {
+  font-family: 'logofont';
+  font-size: 500px !important;
+}
+</style>
