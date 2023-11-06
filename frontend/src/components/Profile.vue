aac16fde87bf0a2f7254216fa85cae91e865bfcf
Nico Haas <nhaas@student.42heilbronn.de>
2023-11-06 11:55:27 +0100

    added profile

--- frontend/src/components/Profile.vue
+++ frontend/src/components/Profile.vue
@@ -2,8 +2,33 @@
 
 </script>
 <template>
- test profil
+    <div class="profile">
+        <div class="hero">
+            <img src="../assets/img/profileCircle.svg" alt="PROFILE PICTURE">
+            <div>
+                <p>FIRSTNAME LASTNAME</p>
+                <p>USERNAME</p>
+            </div>
+        </div>
+        <div>
+        </div>
+    </div>
+    
 </template>
 <style scoped>
 
+    .profile {
+        padding: 10px;
+        border: 0.5px solid white;
+
+    }
+    .hero {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+    }
+
+    img {
+        height: 120px;
+        width: 120px;
+    }
 </style>
