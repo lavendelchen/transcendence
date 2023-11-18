b1c90028d22799daacd6475a6bcf23e8c8beca30
Hoang Long Ngo <hoanglong.ngo@balluff.de>
2023-11-18 14:28:00 +0100

    add enable-tfa endpoint

--- back-end/src/tfa/tfa.controller.ts
+++ back-end/src/tfa/tfa.controller.ts
@@ -1,9 +1,40 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, NotFoundException, Param } from '@nestjs/common';
 import { TfaService } from './tfa.service';
+import { UserService } from 'src/user/user.service';
+
 
 @Controller('tfa')
 export class TfaController {
-  constructor(private readonly tfaService: TfaService) {}
+  constructor(
+    private readonly tfaService: TfaService,
+    private readonly userService: UserService,
+  ) {}
+
+  @Post('enableTfa/:userId')
+  async enableTfa(@Param('userId') userId: number) {
+    // Retrieve the user from the database
+    const user = await this.userService.findOne(userId);
+
+    if (!user) {
+      throw new NotFoundException('User not found.');
+    }
+
+    // Check if TFA is already enabled for the user
+    if (user.is2FActive) {
+      return { message: 'TFA is already enabled for the user.' };
+    }
+
+    // Generate a TFA secret for the user
+    const tfaSecret = this.tfaService.generateTfaSecret();
+
+    // Update the user record in the database
+    const updatedUser = await this.userService.update(userId, {
+      is2FActive: true,
+      secretOf2FA: tfaSecret,
+    });
+
+    return { message: 'TFA enabled for the user.', user: updatedUser };
+  }
 
   @Post('generateOtp')
   async generateOtp(@Body() body) {
@@ -29,6 +60,8 @@ export class TfaController {
     }
   }
 }
+
+
 
 
 //curl -X POST -H "Content-Type: application/json" -d '{"secret": "your_secret_here"}' http://localhost:3000/tfa/generateOtp
--- back-end/src/tfa/tfa.module.ts
+++ back-end/src/tfa/tfa.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TfaController } from './tfa.controller';
 import { TfaService } from './tfa.service';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [TfaController],
   providers: [TfaService],
   exports: [TfaService],
