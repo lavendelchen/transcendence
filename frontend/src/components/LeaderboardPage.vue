b9e92e4e8857b2b91160a1066dd3a40a1e1fab88
Lavendelchen <lavendel02sh@gmail.com>
2023-11-19 02:01:59 +0100

    leaderboard sorted and perfected

--- frontend/src/components/LeaderboardPage.vue
+++ frontend/src/components/LeaderboardPage.vue
@@ -1,8 +1,9 @@
 <template>
 	<h1>Leaderboard</h1>
 	<p v-if="players.length > 0">
-		<span id="img"></span>
+		<span id="rank">Rank</span>
 		<span id="pseudo">Name</span>
+		<span id="img"></span>
 		<span id="legend">Wins</span>
 		<span id="legend">Losses</span>
 		<span id="legend">Total Games</span>
@@ -10,9 +11,10 @@
 		<span id="legend">Points lost</span>
 	</p>
 	<div id="leaderboardBox" v-if="players.length > 0">
-		<p v-for="player in players" :key="player.id" class="leaderboardEntries">
-			<img id="img" :src="player.avatar" alt="Player Avatar"> <!-- make hyperlink? -->
+		<p v-for="player in players" :key="player.rank" class="leaderboardEntries">
+			<span id="rank">{{ player.rank }}</span>
 			<span id="pseudo">{{ player.pseudo }}</span>
+			<img id="img" :src="player.avatar" alt="Player Avatar"> <!-- make hyperlink? -->
 			<span id="wonGames">{{ player.wonMatchesCount }}</span>
 			<span id="lostGames">{{ player.lostMatchesCount }}</span>
 			<span id="playedGames">{{ player.matchesCount }}</span>
@@ -35,6 +37,7 @@
 	  pointsMade: number;
 	  pseudo: string;
 	  wonMatchesCount: number;
+	  rank?: number; // Add the rank property
 	}
 
 	export default {
@@ -48,12 +51,17 @@
 				.then(response => response.json())
 				.then(data => {
 					this.players = data
+		
+					this.players.sort((a: any, b: any) => a.wonMatchesCount + b.wonMatchesCount);
+
 					this.players.forEach((player, index) => {
+			        	player.rank = index + 1;
+				
 						if (player.pseudo.length > 17) {
 							player.pseudo = player.pseudo.slice(0, 17)
 							player.pseudo += "..."
 						}
-					})
+			        });
 				})
 				.catch(error => console.log(error.message))
 		}
@@ -69,7 +77,7 @@
 }
 
 #leaderboardBox {
-	height: 70vh;
+	height: 50vh;
 	overflow: scroll;
 }
 
@@ -84,7 +92,17 @@ span {
 	border-radius: 50%;
 	display: inline-block;
 	vertical-align: middle;
-	padding: 2vw; /* vw or fixed? */
+	margin-right: 2vw;
+	margin-top: 0.8vw;	
+	margin-bottom: 0.8vw;
+}
+
+
+#rank {
+	margin-left: 30px;
+	width: 5vw;
+	font-size: var(--font-size-base);
+	color:rgb(204, 163, 112);
 }
 
 #legend {
@@ -92,7 +110,7 @@ span {
 }
 
 #pseudo {
-	width: 30vw;
+	width: 25vw;
 }
 
 #wonGames {
