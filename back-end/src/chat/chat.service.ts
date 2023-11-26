17193cdd583d73949a2e55a39da2900daa8c0511
Giuliano Jupy <giuliano.jupy@nooxit.com>
2023-11-26 17:11:25 +0100

    feat: 🎸 fix kick and promote | add /add

--- back-end/src/chat/chat.service.ts
+++ back-end/src/chat/chat.service.ts
@@ -12,12 +12,16 @@ export class ChatService extends ChatServiceBase {
     let check = data.input;
     if (data.input.indexOf(' ') != -1)
       check = data.input.substring(0, data.input.indexOf(' '));
+    check = check.toLowerCase();
     switch (check) {
       case '/kick':
         this.kickUser(data);
         break;
       case '/promote':
         this.promoteUser(data);
+        break;
+      case '/add':
+        this.addUserToChannel(data);
         break;
       default:
         this.printMessage(data);
@@ -34,16 +38,38 @@ export class ChatService extends ChatServiceBase {
     }
   }
 
-  private async kickUser(data: IMessage) {
+  private async addUserToChannel(data: IMessage) {
     try {
       const name = data.input.substring(data.input.indexOf(' ') + 1);
-      // only channel admin or owner can kick
-      if (
-        !(this.chatDao.isChannelAdmin(data.room, data.user.name)) &&
-        !(this.chatDao.isChannelOwner(data.room, data.user.name)))
+      const user = await this.userService.findOneByName(name);
+      if (!user) {
+        console.log('user not found');
         return;
-      if (this.chatDao.isChannelOwner(data.room, name)) {
-        console.log('you can\'t kick the owner');
+      }
+      await this.chatDao.addUserToChannel(data.room, name);
+      await this.broadcastToRoom(data, `${name} got added`);
+    } catch (error) {
+      console.log(`SYSTEM: ${error.message.split('\n')[0]}`);
+    }
+  }
+
+  private async kickUser(data: IMessage) {
+    console.log('kick user');
+    try {
+      const name = data.input.substring(data.input.indexOf(' ') + 1);
+      if (
+        !(await this.chatDao.isChannelAdmin(data.room, data.user.name)) &&
+        !(await this.chatDao.isChannelOwner(data.room, data.user.name))) {
+        console.log('you are not admin or owner');
+        return;
+      }
+      if (!(await this.chatDao.isUserInChannel(data.room, name))) {
+        console.log('user is not in channel');
+        return;
+      }
+      console.log('is channel owner: ', this.chatDao.isChannelOwner(data.room, name));
+      if (await this.chatDao.isChannelOwner(data.room, name)) {
+        console.log('you can\'t kick the owner: ');
         return;
       }
       if (name === data.user.name) {
@@ -51,7 +77,7 @@ export class ChatService extends ChatServiceBase {
         return;
       }
       await this.chatDao.removeUserFromChannel(data.room, name);
-      this.broadcastToRoom(data, `${name}: got kicked`);
+      await this.broadcastToRoom(data, `${name} got kicked`);
     } catch (error) {
       console.log(`SYSTEM: ${error.message.split('\n')[0]}`);
     }
@@ -60,12 +86,17 @@ export class ChatService extends ChatServiceBase {
   private async promoteUser(data: IMessage) {
     try {
       const name = data.input.substring(data.input.indexOf(' ') + 1);
-      // only channel admin or owner can promote
       if (
-        !(this.chatDao.isChannelAdmin(data.room, data.user.name)) &&
-        !(this.chatDao.isChannelOwner(data.room, data.user.name)))
+        !(await this.chatDao.isChannelAdmin(data.room, data.user.name)) &&
+        !await (this.chatDao.isChannelOwner(data.room, data.user.name))) {
+        console.log('you are not admin or owner');
         return;
-      if (this.chatDao.isChannelOwner(data.room, name)) {
+      }
+      if (!(await this.chatDao.isUserInChannel(data.room, name))) {
+        console.log('user is not in channel');
+        return;
+      }
+      if (await this.chatDao.isChannelOwner(data.room, name)) {
         console.log('you can\'t promote the owner');
         return;
       }
@@ -74,7 +105,7 @@ export class ChatService extends ChatServiceBase {
         return;
       }
       await this.chatDao.promoteUsertoChannelAdmin(data.room, name);
-      this.broadcastToRoom(data, `${name}: got promoted to admin`);
+      await this.broadcastToRoom(data, `${name} got promoted to admin`);
     } catch (error) {
       console.log(`SYSTEM: ${error.message.split('\n')[0]}`);
     }
